b32bb4c17a367917f97a5b179a85f38657803e9b
Oliver Zott <zott_oliver@web.de>
2021-04-23 17:41:47 +0200

    First example for generics with type constrain

---
+++ Generics/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"{AreEqual("bd", "as")}");
+            Console.WriteLine($"{AreEqual1(23, 23)}");
+            Console.WriteLine($"{AreEqual1(23, 23)}");
+        }
+
+
+        public static bool AreEqual1<T>(T num1, T num2)
+        {
+            return num1.Equals(num2);
+        }
+
+        // more "specific" generic method
+        public static bool AreEqual<T>(T num1, T num2) where T : IComparable
+        {
+            return num1.CompareTo(num2) == 0;
+        }
+    }
+}
