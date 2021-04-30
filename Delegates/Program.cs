3f64055b8a37c32354c6dfe367779aaa2bb59a8d
Oliver Zott <zott_oliver@web.de>
2021-04-30 17:57:40 +0200

    usage of GetInvocationList() and DynamicInvoke() to get all delegate-methods results

--- Delegates/Program.cs
+++ Delegates/Program.cs
@@ -8,20 +8,71 @@ namespace Delegates
     {
 
         public delegate bool Filters(string item);
+        public delegate void Printer(string message);
 
 
         static void Main()
         {
-            String[] names = { "Alice", "Bob", "Siggi", "Magdalena", "Oliver", "Lissi", "Todd", "Al" , "Petra", "Alex"};
 
+            string separator = new String('-', 30);
+
+            #region Example 1
+            Console.WriteLine("\n" + separator + " Example 1 " + separator);
+            String[] names = { "Alice", "Bob", "Siggi", "Magdalena", "Oliver", "Lissi", "Todd", "Al", "Petra", "Alex" };
+
+            // Use delegate as method argument: methods for filtering can be used as variable.
             List<string> resultList = NamesLengthFilter(names, equalsFive);
             List<string> resultList2 = NamesLengthFilter(names, item => item.Length == 4);      // Use Lambda expression instead of methods
-                
-            // methods for filtering can be used as variables in method arguments
-            resultList2.OrderBy(i => i.Length).ToList().ForEach(i => Console.WriteLine($"Length: {i.Length} - {i}"));
+            //resultList2.OrderBy(i => i.Length).ToList().ForEach(i => Console.WriteLine($"Length: {i.Length} - {i}"));
+
+            // Declare and initialize delegate
+            Filters filter = lessThanFive;
+            filter += greateThanFive;
+            filter += equalsFive;
+            //Console.WriteLine(filter("TestWord"));  
+
+            // Only last method returned by delegate ... to get all delegate-methods:
+            List<bool> lengthFilterResult = new();
+            foreach (var del in filter.GetInvocationList())
+            {
+                var result = del.DynamicInvoke("TestWord");
+                lengthFilterResult.Add((bool)result);
+                //Console.WriteLine(result);
+            }
+
+            // get all delegate methods easier with Lambda Expressions
+            List<bool> results = filter.GetInvocationList().Select(del => (bool)del.DynamicInvoke("TestWord")).ToList();
+            Console.WriteLine(String.Join(", ", results));
+
+            #endregion
+
+            #region Example 2
+            Console.WriteLine("\n" + separator + " Example 2 " + separator);
+
+            //assigning Print-method to Printer-delegate and save it in p-delegate value - PUBLISHER
+            Printer p = Print;
+
+            // chaining delegates - SUBSCRIBERS
+            p += PrintTwice;
+            p += Print;
+            p += Print;
+            p -= Print;
+            p("message");
+
+            // show all delegates
+            foreach (var del in p.GetInvocationList())
+            {
+                Console.WriteLine(del.Method);
+            }
+
+            Delegate[] delegates = p.GetInvocationList();
+
+            #endregion
+
         }
 
 
+        #region Example 1 - Simple Delegate implementation with methods and chaining 
         // Filter methods used as delegate
         public static bool lessThanFive(string input)
         {
@@ -52,5 +103,18 @@ namespace Delegates
 
             return result;
         }
+        #endregion
+
+        #region Example 2 - Chaining delegates (methods)
+        public static void Print(string message)
+        {
+            Console.WriteLine(message);
+        }
+        public static void PrintTwice(string message)
+        {
+            Console.WriteLine(message + " - twice");
+            Console.WriteLine(message + " - twice");
+        }
+        #endregion
     }
 }
