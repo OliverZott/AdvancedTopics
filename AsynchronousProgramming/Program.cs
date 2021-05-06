0028fe8a18d411305d23d52dbd21b44f07d475e0
Oliver Zott <zott_oliver@web.de>
2021-05-06 13:29:20 +0200

    Refactoring async method

--- AsynchronousProgramming/Program.cs
+++ AsynchronousProgramming/Program.cs
@@ -8,9 +8,9 @@ namespace AsynchronousProgramming
         private static void Main(string[] args)
         {
             // Task Creation - methods
-            // Task t1 = new Task(ConcatenateChars);
+            // Task t1 = new Task(ConcatenateCharsFirst);
             // t1.Start();
-            // Task t2 = Task.Run(new Action(ConcatenateChars))
+            // Task t2 = Task.Run(new Action(ConcatenateCharsFirst))
 
 
             const int count = 200000;
@@ -18,23 +18,23 @@ namespace AsynchronousProgramming
 
 
             /*
-            var t = Task.Factory.StartNew(() => ConcatenateChars(charToConcatenate, count));
+            var t = Task.Factory.StartNew(() => ConcatenateCharsFirst(charToConcatenate, count));
             Console.WriteLine("In progress (while concatenated)");
             t.Wait();
             Console.WriteLine($"\nCompleted (after concatenated). String length = {t.Result.Length}");
             */
 
             Console.WriteLine("Before starting async method.");
-            var t2 = ConcatenateCharsAsync(charToConcatenate, count);
+            var t = ConcatenateCharsAsync(charToConcatenate, count);
             //t2.Wait();
-            Console.WriteLine("\nAfter starting async method.");
-            Console.WriteLine($"\nMethod two example with Length = {t2.Result.Length}.");
+            Console.WriteLine("After starting async method.");
+            Console.WriteLine($"\nMethod two example with Length = {t.Result.Length}.");
 
 
         }
 
 
-        private static string ConcatenateChars(char charToConcatenate, int count)
+        private static string ConcatenateCharsFirst(char charToConcatenate, int count)
         {
             var concatenatedString = string.Empty;
 
@@ -52,32 +52,34 @@ namespace AsynchronousProgramming
         }
 
 
+        private static string ConcatenateChars(char charToConcatenate, int count)
+        {
+            var concatenatedString = string.Empty;
+
+            for (var i = 0; i < count; i++)
+            {
+                concatenatedString += '1';
+
+                if (i % 10000 == 0)
+                {
+                    Console.WriteLine($"Step {i / 10000}");
+                }
+            }
+
+            return concatenatedString;
+        }
+
+
         private static async Task<string> ConcatenateCharsAsync(char charToConcatenate, int count)
         {
 
-            var t = Task<string>.Factory.StartNew(() =>
-            {
-                var concatenatedString = string.Empty;
-
-                for (var i = 0; i < count; i++)
-                {
-                    concatenatedString += '1';
-
-                    if (i % 10000 == 0)
-                    {
-                        Console.WriteLine($"Step {i/10000}");
-                    }
-                }
-
-                return concatenatedString;
-            });
+            var t = Task<string>.Factory.StartNew(() => ConcatenateChars(charToConcatenate, count));
 
             Console.WriteLine("Inside async method, after Task.Start");
 
             return await t;
 
         }
-
 
 
     }
