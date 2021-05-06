21b1d62a0bb27c54dc7a7970f1eafd2acb927347
Oliver Zott <zott_oliver@web.de>
2021-05-06 13:14:09 +0200

    Add asynchronous example

---
+++ AsynchronousProgramming/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AsynchronousProgramming
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            // Task Creation - methods
+            // Task t1 = new Task(ConcatenateChars);
+            // t1.Start();
+            // Task t2 = Task.Run(new Action(ConcatenateChars))
+
+
+            const int count = 200000;
+            const char charToConcatenate = '-';
+
+
+            /*
+            var t = Task.Factory.StartNew(() => ConcatenateChars(charToConcatenate, count));
+            Console.WriteLine("In progress (while concatenated)");
+            t.Wait();
+            Console.WriteLine($"\nCompleted (after concatenated). String length = {t.Result.Length}");
+            */
+
+            Console.WriteLine("Before starting async method.");
+            var t2 = ConcatenateCharsAsync(charToConcatenate, count);
+            //t2.Wait();
+            Console.WriteLine("\nAfter starting async method.");
+            Console.WriteLine($"\nMethod two example with Length = {t2.Result.Length}.");
+
+
+        }
+
+
+        private static string ConcatenateChars(char charToConcatenate, int count)
+        {
+            var concatenatedString = string.Empty;
+
+            for (var i = 0; i < count; i++)
+            {
+                concatenatedString += '1';
+
+                if (i % 10000 == 0)
+                {
+                    Console.Write('.');
+                }
+            }
+
+            return concatenatedString;
+        }
+
+
+        private static async Task<string> ConcatenateCharsAsync(char charToConcatenate, int count)
+        {
+
+            var t = Task<string>.Factory.StartNew(() =>
+            {
+                var concatenatedString = string.Empty;
+
+                for (var i = 0; i < count; i++)
+                {
+                    concatenatedString += '1';
+
+                    if (i % 10000 == 0)
+                    {
+                        Console.WriteLine($"Step {i/10000}");
+                    }
+                }
+
+                return concatenatedString;
+            });
+
+            Console.WriteLine("Inside async method, after Task.Start");
+
+            return await t;
+
+        }
+
+
+
+    }
+}
